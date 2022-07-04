372da0a8c783a228676b5885ecfd57af3d0b1c7d
NXbi_av4i@itstep.edu.az <NXbi_av4i@itstep.edu.az>
2022-07-04 15:25:29 +0400

    First commit

--- Chat-WPF/MainWindow.xaml.cs
+++ Chat-WPF/MainWindow.xaml.cs
@@ -20,9 +20,46 @@ namespace Chat_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+       
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void usernameText_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (usernameText.Text == "Search")
+            {
+                usernameText.Text = " ";
+            }
+        }
+
+        private void usernameText_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (string.IsNullOrEmpty(usernameText.Text) || string.IsNullOrWhiteSpace(usernameText.Text))
+            {
+                usernameText.Text = "Search";
+            }
+        }
+
+        private void TextBoxCH_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (TextBoxCH.Text == "Write a message...")
+            {
+                TextBoxCH.Text = " ";
+            }
+            else if (TextBoxCH.Text == "")
+            {
+                TextBoxCH.Text = "Write a message...";
+            }
+        }
+
+        private void TextBoxCH_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TextBoxCH.Text) || string.IsNullOrWhiteSpace(TextBoxCH.Text))
+            {
+                TextBoxCH.Text = "Write a message...";
+            }
         }
     }
 }
